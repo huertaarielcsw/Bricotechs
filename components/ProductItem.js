8343489e36466b3af3a03692bdae64de82d95487
huertaarielcsw <huertaarielcsw@gmail.com>
2021-08-26 19:19:06 +0200

    fixed ProductItem

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -10,34 +10,19 @@ import {
 import React from 'react';
 import NextLink from 'next/link';
 import Rating from '@material-ui/lab/Rating';
-import cx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import { useFourThreeCardMediaStyles } from '@mui-treasury/styles/cardMedia/fourThree';
-import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
-
-const useStyles = makeStyles(() => ({
-  root: {
-    maxWidth: 343,
-    margin: 'auto',
-    borderRadius: 12,
-    padding: 12,
-  },
-  media: {
-    borderRadius: 6,
-  },
-}));
+import useStyles from '../utils/styles';
 
 export default function ProductItem({ product }) {
-  const styles = useStyles();
-  const mediaStyles = useFourThreeCardMediaStyles();
-  const shadowStyles = useOverShadowStyles({ inactive: true });
+  const classes = useStyles();
   return (
-    <Card className={cx(styles.root, shadowStyles.root)}>
+    <Card className={classes.card}>
       <NextLink href={`/product/${product.slug}`} passHref>
         <CardActionArea>
           <CardMedia
-            className={cx(styles.media, mediaStyles.root)}
+            className={classes.CardMedia}
+            component="img"
             image={product.image}
+            title={product.name}
           />
           <CardContent>
             <Typography>{product.name}</Typography>
